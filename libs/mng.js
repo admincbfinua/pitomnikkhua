925d8047fc795f338668985ea76ebcb31af7fdde
admincbfinua <mischenkoa@ukr.net>
2017-09-06 16:48:56 +0300

    Update mng.js

--- libs/mng.js
+++ libs/mng.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://pitomnikadmin:jxtymckj;ysqgfhjkm@localhost:27017/pitomnikdb');
+mongoose.connect('mongodb://pitomnikadmin:test;test@localhost:27017/pitomnikdb');
 module.exports = mongoose;
 
 
